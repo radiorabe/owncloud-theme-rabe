8aee6f24f4687c66ccc2099e0f32f2d6db0328a6
Lucas Bickel <hairmare@rabe.ch>
2020-01-24 14:47:50 +0100

    :globe_with_meridians: get rid of german eszett

---
+++ apps/calendar/l10n/de.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "calendar",
+    {
+    "Close" : "Schliessen",
+    },
+    "nplurals=2; plural=(n != 1);");
---
+++ apps/files/l10n/de.js
@@ -0,0 +1,11 @@
+OC.L10N.register(
+    "files",
+    {
+    "Close" : "Schliessen",
+    "Unable to upload {filename} as it is a directory or has 0 bytes" : "Die Datei {filename} kann nicht hochgeladen werden, da sie entweder ein Verzeichnis oder 0 Bytes gross ist",
+    "Size" : "Grösse",
+    "_including %n hidden_::_including %n hidden_" : ["Einschliesslich %n versteckten","Einschliesslich %n versteckten"],
+    "Upload too large" : "Der Upload ist zu gross",
+    "The files you are trying to upload exceed the maximum size for file uploads on this server." : "Die Datei überschreitet die Maximalgrösse für Uploads auf diesem Server.",
+    },
+    "nplurals=2; plural=(n != 1);");
